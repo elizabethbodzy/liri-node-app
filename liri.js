d4067077d68c8b530c21f06464f2642749440519
Elizabeth Bodzy <bodze087@newschool.edu>
2019-08-15 16:56:21 -0500

    Concert search working

--- liri.js
+++ liri.js
@@ -27,16 +27,17 @@ var args = process.argv[2];
 //Finding out info on a concert
 if(args === 'concert-this') {
     var artist = process.argv.slice(3).join(" ");
-    console.log(artist);
+    // console.log(artist);
     
     var request = ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp");
-        console.log(request);
+        // console.log(request);
 
-        // axios.get(request).then(function(artistResponse){
-        //     console.log("Venue: " + artistResponse.data[0].venue.name);
-        //     console.log("City " + artistResponse.data[0].venue.city);
-        //     console.log("Event Date " + moment(artistResponse.data[0].datetime).format("MM/DD/YYYY"));
-        // })
+        axios.get(request).then(function(artistResponse){
+            // console.log(artistResponse)
+            console.log("Venue: " + artistResponse.data[0].venue.name);
+            console.log("City: " + artistResponse.data[0].venue.city);
+            console.log("Event Date: " + moment(artistResponse.data[0].datetime).format("MM/DD/YYYY"));
+        })
 
          //Finding out spotify song info
     } else if (args === 'spotify-this-song') {
@@ -70,31 +71,4 @@ if(args === 'concert-this') {
         })
     }
     
- 
-
-// axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-//   function(response) {
-//     console.log("The movie's rating is: " + response.data.imdbRating);
-//   })
-//   .catch(function(error) {
-//     if (error.response) {
-//       // The request was made and the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log("---------------Data---------------");
-//       console.log(error.response.data);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.status);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       // The request was made but no response was received
-//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
-//       console.log(error.request);
-//     } else {
-//       // Something happened in setting up the request that triggered an Error
-//       console.log("Error", error.message);
-//     }
-//     console.log(error.config);
-//   });
-
 
